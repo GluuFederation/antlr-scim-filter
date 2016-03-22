fe390c01fb0485e2ad7830699c56d25be078f456
uboznikov <uboznikov@outlook.com>
2016-03-22 23:32:11 +0800

    - Fixed grammar - Added tree walker custom listener - Improved test cases

--- src/main/antlr4/org/gluu/antlr/scimFilter/ScimFilter.g4
+++ src/main/antlr4/org/gluu/antlr/scimFilter/ScimFilter.g4
@@ -2,32 +2,49 @@
  * antlr-scim-filter is available under the MIT License (2008). See http://opensource.org/licenses/MIT for full text.
  *
  * Copyright (c) 2016, Gluu
+ *
  * Author: Val Pecaoco
  */
 grammar ScimFilter;
 
+options
+{
+  language = Java;
+}
+
 scimFilter
- : expression EOF
+ : expression* EOF
  ;
 
 expression
- : LPAREN expression RPAREN
- | expression 'and' expression
- | expression 'or' expression
- | ATTRNAME comparator CRITERIA
- | ATTRNAME comparator expression
- | ATTRNAME 'pr'
- | expression ('and' | 'or') 'not' expression
+ : ATTRNAME comparator criteria      # ATTR_COMP_CRITERIA
+ | ATTRNAME comparator expression    # ATTR_COMP_EXPR
+ | ATTRNAME PR                       # ATTR_PR
+ | NOT expression                    # NOT_EXPR
+ | expression AND expression         # EXPR_AND_EXPR
+ | expression OR expression          # EXPR_OR_EXPR
+ | expression comparator expression  # EXPR_COMP_EXPR
+ | LPAREN expression RPAREN          # LPAREN_EXPR_RPAREN
  ;
+
+criteria : DELIMETER (.)+? DELIMETER;
 
 comparator
  : 'eq' | 'ne' | 'co' | 'sq' | 'ew' | 'gt' | 'lt' | 'ge' | 'le'
  ;
 
+NOT : 'not';
+
+AND : 'and';
+OR  : 'or';
+
+PR : 'pr';
+
 LPAREN : '(';
 RPAREN : ')';
 
-ATTRNAME : ('-' | '_' | 'A'..'Z' | 'a'..'z' | '0'..'9' | '.')+;
-CRITERIA : '"' (.)+? '"';
+ATTRNAME  : ('-' | '_' | 'A'..'Z' | 'a'..'z' | '0'..'9' | '.')+;
+
+DELIMETER : '"';
 
 WS : ('\t' | ' ' | '\r' | '\n'| '\u000C')+ -> skip;
--- src/test/java/org/gluu/antlr/scimFilter/FilterTest.java
+++ src/test/java/org/gluu/antlr/scimFilter/FilterTest.java
@@ -6,6 +6,7 @@
 package org.gluu.antlr.scimFilter;
 
 import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
 import org.gluu.antlr.scimFilter.exception.ScimFilterErrorHandler;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -27,18 +28,25 @@ public class FilterTest {
 
         return Arrays.asList(new Object[][] {
             // Valid
+            { true, "userType eq \"emp\"loy\"ee\"" },
             { true, "userType eq \"employee\"" },
-            { true, "userType1234 ne \"Employee\" and email.type eq \"work\"" },
+            { true, "userType1234 ne \"Employee\" and (email.type eq \"work\")" },
+            { true, "(userType1234 ne \"Employee\") or (email.type eq \"work\")" },
+            { true, "userType pr" },
+            { true, "userType pr and not (userType eq \"employee\")" },
+            { true, "not userType eq \"employee\"" },
+            { true, "not (userType eq \"employee\")" }
+            /*
             { true, "userType eq \"employee\" and (email.type eq \"Work\" or email-type eq \"personal\")" },
             { true, "userType pr and ((type eq \"work\" or type eq \"personal\") and user_Type eq \"employee\")" },
-            { true, "userType pr"},
-            { true, "userType eq \"employee\" and userType pr"},
-            { true, "userType pr and (userType eq \"employee\")"},
-            { true, "userType pr and not (userType eq \"employee\")"},
+            { true, "userType eq \"employee\" and userType pr" },
+            { true, "userType pr and userType eq \"employee\"" },
             { true, "(userType pr) and ((type eq \"work\" or type eq \"personal\") and userType eq \"employee\")" },
             { true, "userType pr and (type eq \"work\" or type eq \"personal\") and userType eq \"employee\"" }
+            */
             // Invalid
             /*
+            { true, "user\"Type pr" },
             { true, "pr userType" },
             { true, "pr userType pr" },
             { true, "not userType" },
@@ -58,12 +66,18 @@ public class FilterTest {
     @Test
     public void testFilter() throws Exception {
 
+        // Get lexer
         ANTLRInputStream input = new ANTLRInputStream(this.testFilter);
         ScimFilterLexer lexer = new ScimFilterLexer(input);
-        TokenStream tokens = new CommonTokenStream(lexer);
 
+        // Get list of matched tokens
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+        // Pass tokens to the parser
         ScimFilterParser parser = new ScimFilterParser(tokens);
-
+        parser.setBuildParseTree(true);
+        parser.setTrimParseTree(true);
+        parser.setProfile(true);
         parser.removeErrorListeners();
         parser.setErrorHandler(new ScimFilterErrorHandler());
 
@@ -71,5 +85,8 @@ public class FilterTest {
 
         ParserRuleContext ruleContext = parser.scimFilter();
         assertNull(ruleContext.exception);
+
+        // Walk tree
+        ParseTreeWalker.DEFAULT.walk(new MainFilterListener(), ruleContext);
     }
 }
